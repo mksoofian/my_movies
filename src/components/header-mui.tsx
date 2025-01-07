2de417dd91afb54f4261e7689b48252127fdb821
Mike S <mksoofian@gmail.com>
2025-01-06 19:59:30 -0500

    removed unused components/code

--- src/components/header-mui.tsx
+++ src/components/header-mui.tsx
@@ -43,7 +43,6 @@ function ResponsiveAppBar() {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          {/* <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} /> */}
           <Typography
             variant="h6"
             noWrap
